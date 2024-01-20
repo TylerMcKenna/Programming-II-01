e9ad66070a9bc1a1938780e549d17525ed114d38
TylerMcKenna <tylermckenna01@gmail.com>
2024-01-19 19:39:56 -0600

    Created initial project files

---
+++ ImageTransform/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.imagetransform {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.imagetransform to javafx.fxml;
+    exports com.example.imagetransform;
+}

1e522820b15e01a183c2d1f4c1410b1d7a1535e1
TylerMcKenna <tylermckenna01@gmail.com>
2024-01-21 16:32:46 -0600

    Started work on the user interface

---
+++ ImageTransform/src/main/java/com/example/imagetransform/ImageController.java
@@ -0,0 +1,12 @@
+package com.example.imagetransform;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ImageController {
+
+    @FXML
+    private ImageView imageView;
+
+}
